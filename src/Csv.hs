bc2bf81b9d561110f48ffa6e3a896c18159bed87
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-02-28 23:45:44 +0900

    Change CSV style for MF cloud friendly.

--- src/Csv.hs
+++ src/Csv.hs
@@ -12,26 +12,28 @@ import qualified Data.Vector as Vector
 fromTransactions :: Transactions -> ByteString
 fromTransactions =
   toStrict .
-  Csv.encodeByName transactionHeader . fmap transactionBody . runTransactions
+  Csv.encodeByName transactionHeader . fmap transactionBody . zip [1..] . runTransactions
 
-transactionBody :: Transaction -> HashMap Text Text
-transactionBody Transaction {..} =
+transactionBody :: (Int, Transaction) -> HashMap Text Text
+transactionBody (n, Transaction {..}) =
   HashMap.fromList
-    [ ("日時", tshow tDay)
-    , ("借方", unCategoryName . cName . unDebitCategory $ tDebit)
-    , ("貸方", unCategoryName . cName . unCreditCategory $ tCredit)
+    [ ("取引No", tshow n)
+    , ("取引日", pack $ formatTime defaultTimeLocale "%Y/%m/%d" tDay)
+    , ("借方勘定科目", unCategoryName . cName . unDebitCategory $ tDebit)
+    , ("貸方勘定科目", unCategoryName . cName . unCreditCategory $ tCredit)
     , ("摘要", unDescription tDescription)
     , ("細目", unSubDescription tSubDescription)
-    , ("金額", tshow $ unAmount tAmount)
+    , ("貸借金額", tshow $ unAmount tAmount)
     ]
 
 transactionHeader :: Csv.Header
 transactionHeader =
   Vector.fromList
-    [ encodeUtf8 "日時"
-    , encodeUtf8 "借方"
-    , encodeUtf8 "貸方"
+    [ encodeUtf8 "取引No"
+    , encodeUtf8 "取引日"
+    , encodeUtf8 "借方勘定科目"
+    , encodeUtf8 "貸方勘定科目"
     , encodeUtf8 "摘要"
     , encodeUtf8 "細目"
-    , encodeUtf8 "金額"
+    , encodeUtf8 "貸借金額"
     ]
